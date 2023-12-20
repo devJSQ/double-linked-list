8a3d0c7f406bf144df31cd5c3b33cc7a3fc4e1b7
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-12-20 21:17:23 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+
+        list.displayList();
+    }}
+public class Node {
+    public int data;
+    public Node previous;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.previous = null;
+        this.next = null;
+    }
+}
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+    }
+
+    public void displayList() {
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+/* your code here */
+} 

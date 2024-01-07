b701638f8b8209aed2d7d0e599ec5600d69e4fd6
lamabeta <148013077+lamabeta@users.noreply.github.com>
2024-01-07 13:36:29 +0300

    Create DoublyLinkedList.java

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,25 @@
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertAtEnd(int data) {
+        Node value = new Node(data);
+
+        if (head == null) {
+            head = value;
+            tail = value;
+        } else {
+            tail.next = value;
+            value.previous = tail;
+            tail = value;
+        }
+    }
+
+    /* your code here */
+
+}
